7b0de689e1dfed7348857e48a1e3d45d81b57193
Ravina <ravinakaur@hotmail.com>
2019-07-29 18:15:34 -0700

    Graph appears, edges highlight on click, edge weights appear

--- src/App.js
+++ src/App.js
@@ -11,12 +11,16 @@ class App extends Component {
       weight: -1
     };
 
+    this.setNetworkInstance = this.setNetworkInstance.bind(this)
     this.handleClick = this.handleClick.bind(this);
   }
 
-  handleClick = w => {
+  handleClick = id => {
+    console.log(this.network)
+    this.network.clustering.updateEdge(id, {color : 'red'})
+    
     this.setState({
-      weight: w
+      weight: this.e[id],
     })
   }
 
@@ -28,37 +32,45 @@ class App extends Component {
           { id: 4, label: 'd' },
           { id: 5, label: 'e' }
       ],
-      edges: [{ from: 1, to: 2, label: 5, id: 1}, { from: 1, to: 3, label: 10, id: 2}, { from: 2, to: 4, label:8 }, { from: 2, to: 5, label: 2 }]
+      edges: [{ from: 1, to: 2, label: 5, id: 1}, { from: 1, to: 3, label: 10, id: 2}, { from: 2, to: 4, label:8, id: 3 }, { from: 2, to: 5, label: 2, id: 4 }]
   };
+
+  e = [0, 5, 3, 8, 2]
  
   options = {
       height: "600",
       width: "600",
-      interaction: {dragNodes: false, dragView: false, zoomView:false},
+      interaction: {dragNodes: false, dragView: false, zoomView:false, selectable: false},
+      physics: {enabled:false},
       layout: {
           hierarchical: true
       },
       edges: {
-          color: '#000000'
+          color: 'green',
+          arrows:{to:{enabled: false}},
+          width: "10"
       }
   };
    
   events = {
-      select: function(event) {
-          var { nodes, edges } = event;
-          console.log(edges[0]);
-          if (edges[0]==1){
-                this.handleClick=(5)
-          }
-          
+      click: event => {
+        var {pointer} = event;
+        console.log(event)
+        let edgeId = this.network.getEdgeAt({x: pointer.DOM.x, y: pointer.DOM.y});
+        this.handleClick(edgeId)
+
       }
   };
    
+  setNetworkInstance = nw => {
+    this.network = nw
+  };
+
   render() {
     return (
     <div>
       <h1>DIJKSTRA!!!</h1>
-       <Graph graph={this.graph} options={this.options} events={this.events}></Graph>
+       <Graph getNetwork={this.setNetworkInstance} graph={this.graph} options={this.options} events={this.events}></Graph>
        <p>Points Available: {this.state.weight}</p>
      </div>
     )
