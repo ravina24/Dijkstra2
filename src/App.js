3b187caf096ef8194a1482526107d9288f64f2e3
r6m2b <r6m2b@students.cs.ubc.ca>
2019-08-19 14:57:22 -0700

    moved logic out of render; camelcase

--- src/App.js
+++ src/App.js
@@ -8,10 +8,12 @@ class App extends Component {
     // Constants
     SOLUTION_EDGE_COLOR = 'purple';
     SOLUTION_EDGE_WEIGHT = '5';
+    GENERATED_GRAPH_NUMBER_OF_NODES = 10;
 
     // "Global" variables
 
-    solution_edges = {}; // from Djikstra algorithm
+    solutionEdges = {}; // from Djikstra algorithm
+
     graph = {
         nodes: [
             { id: 1, label: 'a' },
@@ -59,12 +61,11 @@ class App extends Component {
             game_over: false,
             win: false,
             lose: false,
-            solution_edges: null
         };
 
-        // generate graph and run djikstra here
-        generate_graph();
-        run_djikstra();
+        // generate graph and run djikstra to set solution_edges global variable
+        generateGraph();
+        runDjikstra();
 
         this.setNetworkInstance = this.setNetworkInstance.bind(this)
         this.handleClick = this.handleClick.bind(this);
@@ -72,22 +73,37 @@ class App extends Component {
         this.handleLose = this.handleLose.bind(this);
     }
 
-    generate_graph() {
+    // Used by generateGraph
+    generateNodes(){
+      const num = this.GENERATED_GRAPH_NUMBER_OF_NODES;
+
+    }
+
+    // Used by generateGraph
+    generateEdges(nodes){
+
+    }
+
+    generateGraph() {
         // TODO: GENERATE RANDOM GRAPH
-        graph = {
-            nodes: [
-                { id: 1, label: 'a' },
-                { id: 2, label: 'b' },
-                { id: 3, label: 'c' },
-                { id: 4, label: 'd' },
-                { id: 5, label: 'e' }
-            ],
-            edges: [{ from: 1, to: 2, label: 5, id: 1 }, { from: 1, to: 3, label: 10, id: 2 }, { from: 2, to: 4, label: 8, id: 3 }, { from: 2, to: 5, label: 2, id: 4 }]
+        const generatedNodes = generateNodes();
+
+        this.graph = {
+            nodes: generatedNodes,
+            edges: generateEdges(generatedNodes)
         };
     }
 
-    run_djikstra() {
-        solution_edges = djikstra(graph);
+    runDjikstra() {
+        this.solutionEdges = djikstra(this.graph);
+        this.solutionEdges.forEach(edge => sum += edge.weight);
+        this.setState({weight: sum});
+
+
+        // set the edges' color
+        this.solutionEdges.forEach(edge => edge.color = SOLUTION_EDGE_COLOR);
+        // make the edges a bit smaller
+        this.solutionEdges.forEach(edge => edge.width = SOLUTION_EDGE_WEIGHT);
     }
 
 
@@ -109,7 +125,7 @@ class App extends Component {
 
     handleWin() {
         this.setState({
-            game_over = true,
+            gameOver = true,
             win = true,
             lose = false
         })
@@ -117,7 +133,7 @@ class App extends Component {
 
     handleLose() {
         this.setState({
-            game_over = true,
+            gameOver = true,
             win = false,
             lose = true
         })
@@ -126,7 +142,7 @@ class App extends Component {
     // Unused atm
     handleRestart() {
         this.setState({
-            game_over = false,
+            gameOver = false,
             win = false,
             lose = false
         })
@@ -139,41 +155,25 @@ class App extends Component {
     }
 
     // Displays a losing message, plus algorithm solution
-    loseScreen(solution_edges) {
+    loseScreen() {
         // Add the solution's edges to be displayed alongside the user's edges
-        graph.edges.concat(solution_edges);
+        this.graph.edges.concat(this.solutionEdges);
 
         // Display it
         return <div>
             <h1>You lost!</h1>
-            <Graph getNetwork={this.setNetworkInstance} graph={this.state.graph} options={this.options} events={this.events}></Graph>
+            <Graph getNetwork={this.setNetworkInstance} graph={this.graph} options={this.options} events={this.events}></Graph>
         </div>
     }
 
 
     render() {
-        // calculate sum from solution
-        var sum = 0;
-
-        const solution_edges = this.state.solution_edges;
-        solution_edges.forEach(edge => sum += edge.weight);
-        // now we have the sum
-        this.state.weight = sum;
-
-        // call djikstra, get list of edges "solution_edges"
-        solution_edges = djikstra(this.state.graph);
-
-        // set the edges' color
-        solution_edges.forEach(edge => edge.color = SOLUTION_EDGE_COLOR);
-        // make the edges a bit smaller
-        solution_edges.forEach(edge => edge.width = SOLUTION_EDGE_WEIGHT);
-
         // game is over:
-        if (this.state.game_over) {
+        if (this.state.gameOver) {
             if (this.state.win) {
                 return winScreen();
             } else if (this.state.lose) {
-                return loseScreen(solution_edges);
+                return loseScreen();
             } else {
                 throw "PROBLEM: state.game_over is TRUE but both state.win and state.lose are FALSE!";
             }
@@ -183,7 +183,7 @@ class App extends Component {
         else return (
             <div>
                 <h1>DIJKSTRA!!!</h1>
-                <Graph getNetwork={this.setNetworkInstance} graph={this.state.graph} options={this.options} events={this.events}></Graph>
+                <Graph getNetwork={this.setNetworkInstance} graph={this.graph} options={this.options} events={this.events}></Graph>
                 <p>Points Available: {this.state.weight}</p>
             </div>
         )
